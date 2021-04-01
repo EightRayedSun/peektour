ae383c959f07b1746fa188f18bbcc42d2284c73b
Edward Paulo Ganuelas <epganuelas@gmail.com>
2021-03-31 20:19:31 -0400

    add peektour component

---
+++ src/components/Peektour.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="peektour">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PeekTour',
+  props: {
+    images,
+    metaData
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
