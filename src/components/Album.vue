d9d402a3f8d74032cca8b5dda389ea572f983f3d
Edward Paulo Ganuelas <epganuelas@gmail.com>
2021-04-02 15:40:56 -0400

    add album component

---
+++ src/components/Album.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="album">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Album',
+  props: {
+    album: Object
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
