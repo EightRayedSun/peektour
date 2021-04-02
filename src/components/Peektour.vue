c78451b773a35c7d5a582c8e418a9267e8cb2862
Edward Paulo Ganuelas <epganuelas@gmail.com>
2021-04-02 15:38:41 -0400

    vue-router boilerplate

--- src/components/Peektour.vue
+++ src/components/Peektour.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="peektour">
+    <router-link to="/">Home</router-link>
   </div>
 </template>
 
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <peek-tour />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import PeekTour from '@/components/Peektour.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    PeekTour
+  }
+}
+</script>
